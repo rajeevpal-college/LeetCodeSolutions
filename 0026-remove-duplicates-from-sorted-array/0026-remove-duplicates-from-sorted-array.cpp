6b684c7369922891563a2cbd69b25a0d8c4626d1
rajeevpal-college <800rajeevpal@gmail.com>
2026-08-18 15:41:32 +0530

    Time: 0 ms (100%), Space: 22.8 MB (8.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-//   #include <bits/stdc++.h>
-// using namespace std;
-
-// Class to hold the solution logic
-// class Solution {
-// public:
-//     // Function to remove duplicates from sorted array in-place
-    // int removeDuplicates(vector<int>& nums) {
-    //     // If array is empty, return 0 directly
-        if (nums.empty()) return 0;
-
-        // Pointer for the position of last unique element
-        int i = 0;
-
-        // Traverse the array starting from the second element
-        for (int j = 1; j < nums.size(); j++) {
-            // If current element is different from last unique element
-            if (nums[j] != nums[i]) {
-                // Move pointer for unique element forward
-                i++;
-                // Place the new unique element at the next position
-                nums[i] = nums[j];
-            }
-        }
-
-        // i is index of last unique element, count = i + 1
-        return i + 1;
-    }
-};
-
-// // int main() {
-//     vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
-
-//     Solution sol;
-//     int k = sol.removeDuplicates(nums);
-
-//     cout << "Unique count = " << k << "\n";
-//     cout << "Array after removing duplicates: ";
-//     for (int x = 0; x < k; x++) {
-//         cout << nums[x] << " ";
-//     }
-//     cout << endl;
-
-      
+        int i=0;
+        //int n=nums.length;
+        int n=nums.size();
         
+        for( int j=0;j<n;j++){
+            if (nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;}
+            }
+            i++;
+            return i;
+            
+            // return nums ; 
+        }
+        
+};
