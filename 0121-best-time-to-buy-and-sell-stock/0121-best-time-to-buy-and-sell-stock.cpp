f115caf252f6936289a82d5ab2a0572062b0164e
rajeevpal-college <800rajeevpal@gmail.com>
2026-08-29 12:44:07 +0530

    Time: 0 ms (100%), Space: 97.3 MB (87.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,56 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         //difference jisme jyada aaye vo print kr do like 3 loop 
+//         //1 loop se pehla then dusrey se dursa baki se compare kre ga
+//         //
+//         int profit,highestprofit =0;
+//         //n=sizeof(prices) //in c lang
+//         int n=prices.size();
+//         for(int i=0;i<n;i++){
+
+//             for(int j=i;j<n;j++){
+//                profit= prices[j]-prices[i];
+//                if(highestprofit<profit){
+//                 highestprofit=profit;
+
+//                }
+//             }
+//         }
+//         return highestprofit;
+        
+//     }
+// };
+
+
+//ek loop m krna pade ga 
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price = INT_MAX; // Start with the highest possible integer
-        int max_profit = 0;      // If we can't make a profit, we return 0
-
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < min_price) {
-                // Update the minimum price we've seen so far
-                min_price = prices[i];
-            } else if (prices[i] - min_price > max_profit) {
-                // Update the maximum profit if selling today is better
-                max_profit = prices[i] - min_price;
-            }
-        }
+        //difference jisme jyada aaye vo print kr do like 3 loop 
+        //1 loop se pehla then dusrey se dursa baki se compare kre ga
+        //
+        int profit,highestprofit =0;
+        //n=sizeof(prices) //in c lang
+        int n=prices.size();
+        int i=0;
         
-        return max_profit;
+        // int leastBuy=0;//error dega max ka 
+        int leastBuy=prices[0];
+
+        for(int i=0;i<n;i++){
+            if (prices[i]<leastBuy){
+                leastBuy=prices[i];
+            }
+            profit=prices[i]-leastBuy;
+            if (profit>highestprofit){
+                highestprofit=profit;
+
+            }
+
+        
+    }
+    return highestprofit;
     }
 };
