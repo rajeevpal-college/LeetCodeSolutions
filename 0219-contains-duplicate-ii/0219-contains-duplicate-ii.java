0212905ff3caa7ce6a200008b0d5601d056b05c2
rajeevpal-college <800rajeevpal@gmail.com>
2026-09-18 01:02:48 +0530

    Time: 38 ms (6.32%), Space: 124.8 MB (19.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,24 +1,45 @@
-import java.util.HashMap;
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         //int j=nums.length-1;
+//         for(int i=0;i<nums.length;i++){
+//             for (int j=0;j<nums.length;j++){
+//             if(i!=j&&nums[i]==nums[j]&&(Math.abs(i-j)<=k)){
+//             //((i-j<=k)||(j-i<=k))){   //((i-j<=k)&&(j-i>=k))){
+//                 return true;}
+//                 //j--;
+            
+//         }
+//         }
+//         return false;
+        
+        
+//     }
+// }
+
+//capital HashMap, Integer,containsKey,Math.abs()
+//newconcept:HashMap<>map=new HashMap<>();
+//map.conatainKey(),map.get(),map.put,Maths.abs()
+
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (map.containsKey(nums[i])) {
-
-                int prevIndex = map.get(nums[i]);
-
-                if (i - prevIndex <= k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+           
+            if(map.containsKey(nums[i])){
+                int j=map.get(nums[i]);
+                if(Math.abs(i-j)<=k){
                     return true;
                 }
-            }
-
-            map.put(nums[i], i);
+            }    
+            
+              map.put(nums[i],i);
+            
+        
         }
-
         return false;
+        
+        
     }
 }
